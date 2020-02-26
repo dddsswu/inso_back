bb122136414aa32e5a3081f6d343ee7c8792fb97
John_wu <41792109+dddsswu@users.noreply.github.com>
2020-02-26 11:17:03 +0800

    插入留言接口

--- routes/msgbox.js
+++ routes/msgbox.js
@@ -9,8 +9,26 @@ router.get('/', function(req, res, next) {
     query('select * from msgbox',[],function(rows){
         return response.send({data:rows})
     })
-    
 });
+
+router.post('/',function(req,res,next){
+    let response=res;
+    let{msg,username,date}=req.body
+    //date 还需要处理
+    query('insert into msgbox values(?,?,?,?)',[null,msg,username,date],function(rows){
+        let code,msg;
+        if(rows.affectedRows>0){
+            //成功了
+            code=1,
+            msg='提交成功'
+        }else{
+            //失败了
+            code=1,
+            msg='提交失败，稍后再试'
+        }
+        response.send({code,msg})
+    })
+})
 
 
 module.exports = router;
